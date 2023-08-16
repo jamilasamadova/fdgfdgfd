977dcd842204362ebaa1a14c0c7e90dfd16a65bc
jamilasamadova <126696279+jamilasamadova@users.noreply.github.com>
2023-08-16 10:22:38 -0700

    Add files via upload

---
+++ button/script.js
@@ -0,0 +1,56 @@
+// let artir= document.querySelector("artir")
+// let azalt=document.querySelector("azalt")
+// let reqemContent =document.getElementById("reqemContent")
+// let i=0;
+
+
+
+
+
+ 
+
+// let artirReqemi =document.querySelector("artir");
+// let azaltReqemi=document.querySelector("azalt");
+// artirReqemi.addEventListener("click", function(){
+//     console.log('artir')
+
+//     i++;
+//     reqemContent.innerText=i;
+// })
+// azaltReqemi.addEventListener("click", function() {
+//     console.log('azalt')
+//     i--;
+//     reqemContent.innerText=i;
+// })
+
+// goster.addEventListener('click', function(){
+//     for ()
+//     console.log ("gizledildi");
+
+
+
+//     content.style.display='block'
+// })
+
+// goster.addEventListener('click', function(){
+//     console.log ("gizledildi");
+//     content.style.display='none'
+// })
+
+
+
+let data=''
+let names=["Tamerlan", "Kerim" , "Jamila" , "Nicat"]
+let name = document.getElementById("name")
+let listContent=document.getElementById("listContent")
+
+
+names.forEach(element => {
+    console.log ([i])
+    listContent.innerHTML=
+});
+
+
+
+
+
